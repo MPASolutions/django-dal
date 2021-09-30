6f503b193498d86419e158c5167b56f9bd6d8e11
Alessandro Regolini <regolini@mpasol.it>
2021-09-30 14:01:50 +0200

    better 403 message

--- django_dal/utils.py
+++ django_dal/utils.py
@@ -10,16 +10,19 @@ class HttpResponseForbiddenInfo(HttpResponse):
     def __init__(self, *args, **kwargs):
         if 'perm_code' in kwargs:
             self.perm_code = kwargs.pop('perm_code')
+        if 'user' in kwargs:
+            self.user = kwargs.pop('user')
         super().__init__(*args, **kwargs)
         del self['content-type']
 
     @HttpResponse.content.setter
     def content(self, value):
-        raise AttributeError("403 Forbidden: {}".format(self.perm_code))
+        raise AttributeError("403 Forbidden: permission {}, user {}".format(self.perm_code, self.user))
 
 
 def check_permission(model, perm_name):
     perm_code = f'{model._meta.app_label}.{perm_name}_{model._meta.model_name}'
     if cxpr.user is not None:
         if not cxpr.user.has_perm(perm_code):
-            raise HttpResponseForbiddenInfo(**{'perm_code':perm_code})
+            raise HttpResponseForbiddenInfo(**{'perm_code': perm_code,
+                                               'user': cxpr.user})
