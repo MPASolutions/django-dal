6e3bd443fc94d002469208948a4928195c4ced12
Alessandro Regolini <regolini@mpasol.it>
2023-11-07 17:20:50 +0100

    fix-model-is-none+enable-annotation-same-field-name

--- django_dal/query.py
+++ django_dal/query.py
@@ -1,8 +1,12 @@
+from itertools import chain
+
 from django.db.models.query import QuerySet
-from django_dal.utils import check_permission
-from mptt.querysets import TreeQuerySet
+from django.db.models.query_utils import FilteredRelation
 from django.db.models.utils import resolve_callables
-from mptt import utils
+from mptt.querysets import TreeQuerySet
+
+from django_dal.utils import check_permission
+
 
 class DALQuerySet(QuerySet):
 
@@ -36,6 +40,60 @@ class DALQuerySet(QuerySet):
             setattr(obj, k, v)
         obj.save(using=self.db)
         return obj, False
+
+    def _annotate(self, args, kwargs, select=True):
+        self._validate_values_are_expressions(
+            args + tuple(kwargs.values()), method_name="annotate"
+        )
+        annotations = {}
+        for arg in args:
+            # The default_alias property may raise a TypeError.
+            try:
+                if arg.default_alias in kwargs:
+                    raise ValueError(
+                        "The named annotation '%s' conflicts with the "
+                        "default name for another annotation." % arg.default_alias
+                    )
+            except TypeError:
+                raise TypeError("Complex annotations require an alias")
+            annotations[arg.default_alias] = arg
+        annotations.update(kwargs)
+
+        clone = self._chain()
+        names = self._fields
+        if names is None:
+            names = set(
+                chain.from_iterable(
+                    (field.name, field.attname)
+                    if hasattr(field, "attname")
+                    else (field.name,)
+                    for field in self.model._meta.get_fields()
+                )
+            )
+
+        for alias, annotation in annotations.items():
+            #            if alias in names:
+            #                raise ValueError(
+            #                    "The annotation '%s' conflicts with a field on "
+            #                    "the model." % alias
+            #                )
+            if isinstance(annotation, FilteredRelation):
+                clone.query.add_filtered_relation(annotation, alias)
+            else:
+                clone.query.add_annotation(
+                    annotation,
+                    alias,
+                    select=select,
+                )
+        for alias, annotation in clone.query.annotations.items():
+            if alias in annotations and annotation.contains_aggregate:
+                if clone._fields is None:
+                    clone.query.group_by = True
+                else:
+                    clone.query.set_group_by()
+                break
+
+        return clone
 
 
 class DALTreeQuerySet(TreeQuerySet, DALQuerySet):
