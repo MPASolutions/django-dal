a1864c85390fb77867abbbf8802d8a72f1fff536
Leonardo Perathoner <perathoner@mpasol.eu>
2024-03-06 14:48:40 +0000

    jwt authorization

--- django_dal/utils_authentication.py
+++ django_dal/utils_authentication.py
@@ -1,10 +1,10 @@
-
+import base64
 import binascii
 
-import base64
 from django.contrib.auth import login, authenticate
 
 from django_dal.params import get_context_params
+from django_dal.utils_iwt import decode_jwt
 
 
 def get_basic_auth(request):
@@ -45,6 +45,15 @@ def get_basic_auth(request):
 
 def authorize(request):
     if not request.user.is_authenticated:
+
+        # check JWT
+        jwt_authorized, jwt_payload, msg = decode_jwt(request, jwt_required=False)
+        # with jwt_required=False it returns jwt_authorized as False only if really fails to decode/expired/invalid
+        # if no JWT was sent in the request or the audience is not configured to receive it, it returns 
+        # anyway jwt_authorized True (but jwt_payload is None)
+        if jwt_authorized is False:
+            return False, msg
+
         username, password, msg = get_basic_auth(request)
         if msg is not None:
             return False, msg
@@ -57,6 +66,15 @@ def authorize(request):
 
         if not request.user.is_authenticated:
             return False, 'User is not authenticated'
+
+        # add the issuer session to the new session (without overwriting the orginal authenticaiton keys)
+        # if it had been done earlier (where jwt_authorized is checked) it would have been overwritten by authenticate/login
+        # so I have to do it here at the end with the caution not to overwrite the keys of the orginal authentication
+        if jwt_payload is not None:
+            request.session.update(
+                {k: v for k, v in jwt_payload.items() if
+                 k not in ['_auth_user_id', '_auth_user_backend', '_auth_user_hash']}
+            )
 
     context_params = get_context_params()
     context_params.set_to_none()
