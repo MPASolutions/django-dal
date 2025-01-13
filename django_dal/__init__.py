8ec1434e0a09b0827355a9855de1035b7a01a1b5
Alessandro Regolini <alessandro.regolini@enogis.it>
2025-01-13 20:17:30 +0000

    Update __init__.py

--- django_dal/__init__.py
+++ django_dal/__init__.py
@@ -1,5 +1,5 @@
 from django_dal.params import ContextParam, ContextParams, cxpr  # noqa: F401
 
-__version__ = "24.12.0"
+__version__ = "25.1.0"
 
 default_app_config = "django_dal.apps.DjangoDALConfig"
